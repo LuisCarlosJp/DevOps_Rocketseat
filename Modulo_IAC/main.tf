847230886c63ac4c0d4baff4330879b3a74df750
luiscarlosjp <jardimpereiralc@gmail.com>
2025-06-29 23:15:45 -0300

    Workspaces

--- Modulo_IAC/main.tf
+++ Modulo_IAC/main.tf
@@ -1,8 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "luiscarlosjp-bucket-iac"
+  bucket = "luiscarlosjp-bucket-iac-${terraform.workspace}"
 
   tags = {
     Name = "First Bucket"
     Iac = true
+    context = "${terraform.workspace}"
   }
 }
