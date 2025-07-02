0fe70b1810c0396b5c663aca19ed06c04b06d076
luiscarlosjp <jardimpereiralc@gmail.com>
2025-07-02 14:29:29 -0300

    add variables

--- Modulo_IAC/datasources.tf
+++ Modulo_IAC/datasources.tf
@@ -1,3 +1,3 @@
 data "aws_s3_bucket" "bucket" {
-  bucket = "luiscarlosjp-bucket-iac-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-${terraform.workspace}" 
 }
--- Modulo_IAC/main.tf
+++ Modulo_IAC/main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "luiscarlosjp-bucket-iac-${terraform.workspace}"
+  bucket = "${var.org_name}-bucket-iac-${terraform.workspace}"
 
   tags = {
     Name = "First Bucket"
