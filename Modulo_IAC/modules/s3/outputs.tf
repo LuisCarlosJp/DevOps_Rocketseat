285152484345f5681373cca77eadab5a927d25d3
luiscarlosjp <jardimpereiralc@gmail.com>
2025-07-04 22:42:37 -0300

    chore: fix lint issues

--- Modulo_IAC/modules/s3/outputs.tf
+++ Modulo_IAC/modules/s3/outputs.tf
@@ -1,5 +1,11 @@
 output "bucket_domain_name" {
-  value = ""
-  sensitive = false
+  value       = aws_s3_bucket.bucket.bucket_domain_name
+  sensitive   = false
   description = "Nome do domínio do bucket S3"
+}
+
+output "bucket_id" {
+  value       = aws_s3_bucket.bucket.id
+  sensitive   = false
+  description = "Id do domínio do bucket S3"
 }
--- Modulo_IAC/modules/s3/variables.tf
+++ Modulo_IAC/modules/s3/variables.tf
@@ -1,4 +1,4 @@
 variable "s3_bucket_name" {
-  type = string
+  type        = string
   description = "Nome do bucket"
 }
